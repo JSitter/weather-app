62cf3c3bc95950035069a60648a32b463379876b
Justin Sitter <justin@jaytria.com>
2018-03-07 11:39:37 -0800

    added components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './cloud.png';
+import Header from './header'
+import Weather from './components/weather'
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Bloop</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header title="Welcom to Bloop" />
+        <Weather api-key={process.env.API_KEY}/> 
       </div>
     );
   }
---
+++ src/components/weather/components/weather.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import GeoLocation from './geolocation'
+class Weather extends Component{
+    render(){
+        return(
+            <div>
+                <p className="App-intro">
+                    Great weather app you got there. Try Alpo!
+                </p>
+            </div>
+            
+        )
+    }
+}
+
+export default Weather;
