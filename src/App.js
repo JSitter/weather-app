51edae1111119308538534593f806062b68f5403
Justin Sitter <justin@jaytria.com>
2018-03-09 00:02:35 -0800

    hits api point

--- src/App.js
+++ src/App.js
@@ -5,12 +5,38 @@ import './App.css';
 
 
 class App extends Component {
+
+  state = {
+    response: 'easy'
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({"response": res}))
+      .catch(err => console.log(err));
+  }
+
+  // callApi = ()=>{
+
+  //   fetch('/api/spark').then((res)=>{
+  //     this.setState({"response": res})
+  //   })
+  // }
+  callApi = async () => {
+    const response = await fetch('/api/spark');
+    const body = await response.text();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
   render() {
     return (
       <div className="App">
         <Header title="Welcom to Bloop" />
         <Weather apikey={process.env.REACT_APP_WEATHER_KEY}/> 
-
+        <p>hello{this.state.response}</p>
       </div>
     );
   }
