90ccf62498c8cb1d16a2d58577afe6a86614acb0
Justin Sitter <justin@jaytria.com>
2018-03-10 02:36:20 -0800

    added location city name lookup

--- src/components/locate.js
+++ src/components/locate.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
 
 class Locate extends Component{
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            location:""
+        }
+    }
     render(){
+        let handleGetLocation=()=>{
+            console.log("clicked")
+            if("geolocation" in navigator){
+                this.setState({location: "searching"});
+                fetchLocation();
+            } else{
+                this.setState({ status: "Geolocation is currently available."})
+                return
+            }
+        }
+    
+        let fetchLocation=()=>{
+            navigator.geolocation.getCurrentPosition((position)=>{
+                fetch("https://maps.googleapis.com/maps/api/geocode/json?address=")
+                .then((response)=>response.json())
+                .then((json)=>{
+                    console.log(json)
+                })
+                this.props.handleLocationChange(position)
+                this.setState({"location": ""})
+               
+            })
+        }
+
         return (
             <div className="input-wrap">
-                       <a onClick={()=>this.props.handleLocationChange("hello")}> 
+                       <a className={this.state.location} onClick={handleGetLocation}> 
                            <i className="material-icons">my_location</i>
                        
                        </a>
